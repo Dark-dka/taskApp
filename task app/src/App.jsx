7f134ace3068d39dde5b76e0e8a0d4dac5478bac
Dark-dka <73824592+Dark-dka@users.noreply.github.com>
2023-09-08 23:38:49 +0500

    вцф

--- task app/src/App.jsx
+++ task app/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './companents/navbar/Navbar'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 
